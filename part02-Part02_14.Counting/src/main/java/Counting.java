8b1f32f4be74559f0f8de144420e3e288230e79d
Myami <myaminored@gmail.com>
2025-07-14 17:52:12 -0500

    test

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,11 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = Integer.valueOf(scanner.nextLine());
+        int counter = 0 ;
+        while (counter <= number ){
+            System.out.println(counter);
+            counter++;
+        }
     }
 }

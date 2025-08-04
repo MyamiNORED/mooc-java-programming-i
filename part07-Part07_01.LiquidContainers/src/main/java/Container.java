3dc4c6ed95254b5c4f170e3fba15e5d4892bfc23
myami <myaminored@gmail.com>
2025-08-04 22:37:37 +0100

    Auto-sync mooc-java-programming-i at -08-2025 10:37:pm

--- part07-Part07_01.LiquidContainers/src/main/java/Container.java
+++ part07-Part07_01.LiquidContainers/src/main/java/Container.java
@@ -9,5 +9,38 @@
  * @author Myami
  */
 public class Container {
+    private String name ;
+    private int maxCapacity ;
+
+    public Container(String name) {
+        this.name = name;
+        this.maxCapacity = 0;
+    }
     
+    public void add (int amount){
+        int toAdd = this.maxCapacity + amount ;
+        if(toAdd > 100){
+        
+            this.maxCapacity = 100 ;
+            return;
+        }
+    
+        this.maxCapacity += amount ;
+    }
+    
+    public void move (int amount,Container B){
+        if(amount > maxCapacity){
+            this.maxCapacity = 0;
+            B.add(amount);
+            return;
+        }
+        this.maxCapacity -= amount ;
+        B.add(amount);
+    }
+    public void remove (int amount, Container B){ 
+        B.move(amount, B);
+    }
+    public String toString(){
+        return this.name +": "+this.maxCapacity+"/100";
+    }
 }
--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,7 +5,8 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container a = new Container("First");
+        Container b = new Container("Second");
 
         while (true) {
             System.out.print("> ");
@@ -13,6 +14,15 @@ public class LiquidContainers {
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }
+            System.out.println(a.toString());
+            System.out.println(b.toString());
+            if (input.equals("add")) {
+                System.out.println("add ");
+                int num = Integer.valueOf(scan.nextLine());
+                a.add(num);
+                System.out.println(a.toString());
+                System.out.println(b.toString());
             }
 
         }
